15331f1eaed752fc6c19608606b012cb219fe9ee
stormy_ <stormy.hs@protonmail.com>
2022-01-06 23:00:46 +0100

    whoops

--- configs.py
+++ configs.py
@@ -8,7 +8,3 @@ logFormat = "log"  # be sure not to put a dot in here
 if(token == ""):
     print("Please enter a discord token")
     sys.exit(0)
-
-if(id == ""):
-    print("Please enter a user ID corresponding to the token")
-    sys.exit(0)
